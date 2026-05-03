9cade6dd85cc5775a2955f4ee8ab13795e94ccbc
Mohamed Ibrahim <mohamedibrahm64@gmail.com>
2026-05-03 21:56:51 +0300

    Initialize Vehicle class

---
+++ src/com/transportation/model/Vehicle.java
@@ -0,0 +1,13 @@
+package com.transportation.model;
+
+public class Vehicle {
+    private String licensePlate;
+    private String model;
+
+    public Vehicle(String licensePlate, String model) {
+        this.licensePlate = licensePlate;
+        this.model = model;
+    }
+
+    // Getters and setters
+}
