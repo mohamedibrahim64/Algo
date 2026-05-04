b45bd77c35d9951a526e7368cd88fc523960c793
Mohamed Ibrahim <mohamedibrahm64@gmail.com>
2026-05-04 23:33:58 +0300

    Create Node.java

---
+++ src/main/java/com/transportation/dataStructures/Node.java
@@ -0,0 +1,22 @@
+package com.transportation.dataStructures;
+
+/**
+ * Represents a node in the graph.
+ */
+public class Node {
+    private int id;
+    private String name;
+
+    public Node(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
